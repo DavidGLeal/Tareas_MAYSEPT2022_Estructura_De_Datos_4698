27fcadfb62f4a4eeed66028a768476f9292f64b5
Camila Rivera <camila_2620@hotmail.com>
2022-08-19 18:20:12 -0500

    Codigo

---
+++ mainPrincipal.cpp
@@ -0,0 +1,23 @@
+/*
+	*Universidad de las Fuerzas Armadas "ESPE"
+    *Enunciado del problema:
+	    *Programa que realize los diferentes tipos de ordenamientos (internos y externos) y busquedas.
+     *Autores:
+        *Curso de Estructura de Datos NRC: 4698
+	*Fecha de creacion: 
+        * 21-06-2022
+	*Fecha de modificacion:
+        * 28-06-2022
+    *GitHub del grupo:	
+        *https://github.com/DavidGLeal/4698_Ordenamientos
+*/
+
+#include <iostream>
+#include "Menu.h"
+
+int main(int argc, char** argv) {
+	
+	MenuPrincipal();
+	
+	return 0;
+}
